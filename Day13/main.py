0b8bb2a94a22b317c501184b5d0a82549004c0c4
Erik Fagerli <Erik-Thallaug.Fagerli@ffi.no>
2022-12-23 10:47:08 +0100

    solve part 2 Day13

--- Day13/main.py
+++ Day13/main.py
@@ -30,10 +30,28 @@ def f(x, y):
 
 t = 0
 
-# a and b are each of the list that need to be compared
 for i, (a, b) in enumerate(x):
-    # check if first list is smaller than second list, if so add index + 1 to total. eval() converst the string format to int format "[1, 2, 3]" -> [1, 2, 3]  
-    if f(eval(a), eval(b)) < 0:
+    v = f(eval(a), eval(b))
+    if v < 0:
         t += i + 1
-    
 print(t)
+
+# Part 2
+# Create list where each item is a list in text file
+x = list(map(eval, open("text.txt").read().split()))
+
+
+i2 = 1
+i6 = 2
+
+# Loop through each list
+for a in x:
+    # Use the function in part 1 to check if the list is less than the divider packets. 
+    # If so add values to i2, and/or i6.
+    if f(a, [[2]]) < 0:
+        i2 += 1
+        i6 += 1
+    elif f(a, [[6]]) < 0:
+        i6 += 1
+
+print(i2 * i6)
