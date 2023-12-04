1d6a0768d1a286f648ce86e672ccd7b0d4009127
Erik Fagerli <Erik-Thallaug.Fagerli@ffi.no>
2023-12-04 08:46:16 +0100

    day_01 part 2 complete

--- 2023/Day_01/day_01.py
+++ 2023/Day_01/day_01.py
@@ -3,23 +3,6 @@ import re
 
 sum = 0
 
-def convert_to_numbers(s):
-   
-    words_to_numbers = {
-        'one': '1',
-        'two': '2',
-        'three': '3',
-        'four': '4',
-        'five': '5',
-        'six': '6',
-        'seven': '7',
-        'eight': '8',
-        'nine': '9',
-        'zero': '0'
-    }
-    pattern = re.compile(r'\b(' + '|'.join(words_to_numbers.keys()) + r')\b')
-    return re.sub(pattern, lambda x: words_to_numbers[x.group()], s)
-
 # Part 1
 with open("text_file.txt") as f:
     for line in f:
@@ -33,8 +16,87 @@ print(sum)
 
 # Part 2
 
+words_to_numbers = {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+        'zero': '0'
+    }
 
-with open("example.txt") as f:
+sum_2 = 0
+
+with open("text_file.txt") as f:
     for line in f:
-        lst = []
-        print(convert_to_numbers(line))
+        new_lst = []
+        new_line = line.strip()
+        for idx in range(1,len(line)):
+            if line[:idx][-1].isdigit():
+                new_lst.append(line[:idx][-1])
+                break
+            elif "one" in line[:idx]:
+                new_lst.append("1")
+                break
+            elif "two" in line[:idx]:
+                new_lst.append("2")
+                break
+            elif "three" in line[:idx]:
+                new_lst.append("3")
+                break
+            elif "four" in line[:idx]:
+                new_lst.append("4")
+                break
+            elif "five" in line[:idx]:
+                new_lst.append("5")
+                break
+            elif "six" in line[:idx]:
+                new_lst.append("6")
+                break
+            elif "seven" in line[:idx]:
+                new_lst.append("7")
+                break
+            elif "eight" in line[:idx]:
+                new_lst.append("8")
+                break
+            elif "nine" in line[:idx]:
+                new_lst.append("9")
+                break
+        for idx in range(len(new_line)-1,-1,-1):
+            if new_line[idx].isdigit():
+                new_lst.append(new_line[idx])
+                break
+            elif "one" in new_line[idx:]:
+                new_lst.append("1")
+                break
+            elif "two" in new_line[idx:]:
+                new_lst.append("2")
+                break
+            elif "three" in new_line[idx:]:
+                new_lst.append("3")
+                break
+            elif "four" in new_line[idx:]:
+                new_lst.append("4")
+                break
+            elif "five" in new_line[idx:]:
+                new_lst.append("5")
+                break
+            elif "six" in new_line[idx:]:
+                new_lst.append("6")
+                break
+            elif "seven" in new_line[idx:]:
+                new_lst.append("7")
+                break
+            elif "eight" in new_line[idx:]:
+                new_lst.append("8")
+                break
+            elif "nine" in new_line[idx:]:
+                new_lst.append("9")
+                break
+        print(new_lst)
+        sum_2 += int("".join(new_lst))
+print(sum_2)
