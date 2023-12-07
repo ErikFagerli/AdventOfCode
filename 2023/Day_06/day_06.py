75fcc8e2c3103ced58c101eac9bfb525d625c783
Erik Fagerli <Erik-Thallaug.Fagerli@ffi.no>
2023-12-07 11:17:47 +0100

    day_06 part 2 complete

--- 2023/Day_06/day_06.py
+++ 2023/Day_06/day_06.py
@@ -1,23 +1,15 @@
 
 time, distance = open("text_file.txt").read().split("\n")
 
-time = list(map(int, time.split(":")[1].strip().split()))
-distance = list(map(int, distance.split(":")[1].strip().split()))
+time = int("".join(list(map(str, time.split(":")[1].split()))))
+distance = int("".join(list(map(str, distance.split(":")[1].split()))))
 
-different_options = []
-for max_time, record_distance in zip(time, distance):
-    count = 0
-    for charge_time in range(max_time):
-        remaining_time = max_time - charge_time
-        distance_travelled = charge_time * remaining_time
-        if distance_travelled > record_distance:
-            count += 1
-    different_options.append(count)
-print(different_options)
+count = 0
+for charge_time in range(time):
+    remaining_time = time - charge_time
+    distance_travelled = charge_time * remaining_time
+    if distance_travelled > distance:
+        count += 1
+print(count)
 
-sum = 1
-
-for num in different_options:
-    sum *= num
-print(sum)
 
