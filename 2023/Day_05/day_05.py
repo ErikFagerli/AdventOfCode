1a3289b585bed82d3b7e25569d2fa5f68a3c6ea0
Erik Fagerli <Erik-Thallaug.Fagerli@ffi.no>
2023-12-07 10:43:15 +0100

    day_05 part 2 complete

--- 2023/Day_05/day_05.py
+++ 2023/Day_05/day_05.py
@@ -1,20 +1,30 @@
-seeds, *blocks = open("example.txt").read().split("\n\n")
+inputs, *blocks = open("text_file.txt").read().split("\n\n")
 
-seeds = list(map(int, seeds.split(": ")[1].split()))
+inputs = list(map(int, inputs.split(": ")[1].split()))
 
-print(seeds)
+seeds = []
+
+for i in range(0, len(inputs), 2):
+    seeds.append((inputs[i], inputs[i] + inputs[i+1]))
 
 for block in blocks:
     ranges = []
     for line in block.splitlines()[1:]:
         ranges.append(list(map(int, line.split())))
-    new = []
-    for seed in seeds:
+    new_pos = []
+    while seeds:
+        start, end = seeds.pop()
         for destination, source, range_len in ranges:
-            if source <= seed < source + range_len:
-                new.append(seed - source + destination)
+            overlap_start = max(start, source)
+            overlap_end = min(end, source + range_len)
+            if overlap_start < overlap_end:
+                new_pos.append((overlap_start - source + destination, overlap_end - source + destination))
+                if overlap_start > start:
+                    seeds.append((start, overlap_start))
+                if overlap_end < end:
+                    seeds.append((overlap_end, end))
                 break
-        else:
-            new.append(seed)
-    seeds = new
-print(min(seeds))
+        else: 
+            new_pos.append((start, end))
+    seeds = new_pos
+print(min(seeds)[0])
